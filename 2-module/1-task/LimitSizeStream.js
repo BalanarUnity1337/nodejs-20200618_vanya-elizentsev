e6d79b5074b89411a1880d6f5d753f70b00a4952
Vanya Elizentsev <vanesdemon96@gmail.com>
2020-06-27 17:54:07 +0700

    Стрим с лимитом передачи данных.

--- 2-module/1-task/LimitSizeStream.js
+++ 2-module/1-task/LimitSizeStream.js
@@ -1,13 +1,23 @@
-const stream = require('stream');
+const {Transform} = require('stream');
 const LimitExceededError = require('./LimitExceededError');
 
-class LimitSizeStream extends stream.Transform {
+class LimitSizeStream extends Transform {
   constructor(options) {
     super(options);
+
+    this.setEncoding(options.defaultEncoding);
+    this._limit = options.limit;
+    this._fullLength = 0;
   }
 
   _transform(chunk, encoding, callback) {
+    if (this._fullLength === this._limit) {
+      callback(new LimitExceededError());
+    } else {
+      this._fullLength += chunk.toString().length;
 
+      callback(null, chunk);
+    }
   }
 }
 
