3133743a75e0371f430ea08226f60f76c946b122
Vanya Elizentsev <vanesdemon96@gmail.com>
2020-07-05 17:04:37 +0700

    Чат.

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -5,13 +5,44 @@ const app = new Koa();
 app.use(require('koa-static')(path.join(__dirname, 'public')));
 app.use(require('koa-bodyparser')());
 
-const Router = require('koa-router');
+const Router = require('@koa/router');
 const router = new Router();
 
+const subscribers = new Set();
+
 router.get('/subscribe', async (ctx, next) => {
+  const subscriber = new Promise((resolve, reject) => {
+    subscribers.add(resolve);
+
+    app.on('error', (err) => {
+      subscribers.delete(resolve);
+      reject(err);
+    });
+  });
+
+  try {
+    const message = await subscriber;
+
+    ctx.response.status = 200;
+    ctx.response.body = message;
+  } catch (e) {
+    throw e;
+  }
 });
 
 router.post('/publish', async (ctx, next) => {
+  const message = ctx.request.body.message;
+
+  if (message) {
+    subscribers.forEach((subscriber) => {
+      subscriber(message);
+    });
+
+    subscribers.clear();
+
+    ctx.response.status = 200;
+    ctx.response.body = message;
+  }
 });
 
 app.use(router.routes());
