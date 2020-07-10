d10790b52acbe3f8c99009a895eac1ed80c09526
Vanya Elizentsev <vanesdemon96@gmail.com>
2020-07-10 22:46:24 +0700

    Онлайн-магазин (товары и категории).

--- 6-module/2-task/app.js
+++ 6-module/2-task/app.js
@@ -3,6 +3,25 @@ const Router = require('koa-router');
 const {productsBySubcategory, productList, productById} = require('./controllers/products');
 const {categoryList} = require('./controllers/categories');
 
+const categoryModel = require('./models/Category');
+const productModel = require('./models/Product');
+
+// productModel.create({
+//   title: 'Product1',
+//   description: 'Description1',
+//   price: 10,
+//   category: '5f07479c937073409cc25e1b',
+//   subcategory: '5f07479c937073409cc25e1c',
+//   images: ['image1'],
+// });
+
+// categoryModel.create({
+//   title: 'Category1',
+//   subcategories: [{
+//     title: 'Subcategory1',
+//   }],
+// });
+
 const app = new Koa();
 
 app.use(async (ctx, next) => {
