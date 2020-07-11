b62820326342481b39a557b1697db41d22b5749a
Vanya Elizentsev <vanesdemon96@gmail.com>
2020-07-11 19:08:58 +0700

    Настройка локальной стратегии.

--- 7-module/1-task/libs/strategies/local.js
+++ 7-module/1-task/libs/strategies/local.js
@@ -1,8 +1,19 @@
+const userModel = require('../../models/User');
 const LocalStrategy = require('passport-local').Strategy;
 
 module.exports = new LocalStrategy(
     {usernameField: 'email', session: false},
-    function(email, password, done) {
-      done(null, false, 'Стратегия подключена, но еще не настроена');
+    async function(email, password, done) {
+      const user = await userModel.findOne({email});
+
+      if (!user) {
+        done(null, false, 'Нет такого пользователя');
+      } else {
+        if (!await user.checkPassword(password)) {
+          done(null, false, 'Неверный пароль');
+        } else {
+          done(null, user, 'OK');
+        }
+      }
     }
 );
